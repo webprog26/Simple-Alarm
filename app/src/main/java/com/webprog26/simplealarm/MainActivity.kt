6bd091d0af5c576f9bf577bd312e6b552834fb96
Volodymyr Shalashenko <webprog26@gmail.com>
2026-02-23 18:15:13 +0200

    RecyclerView to show list of active alarms added (#2)

--- app/src/main/java/com/webprog26/simplealarm/MainActivity.kt
+++ app/src/main/java/com/webprog26/simplealarm/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         mainViewModel.mButtonClicked.observe(this, Observer { item ->
-
         })
     }
 }
---
+++ app/src/main/java/com/webprog26/simplealarm/data/Alarm.kt
@@ -0,0 +1,7 @@
+package com.webprog26.simplealarm.data
+
+data class Alarm (
+    var hour: Int,
+    var minute: Int,
+    var isActive: Boolean
+)
